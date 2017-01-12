182711afd1082962229fbf417a1636bb0f30903e
David <46406163y@iespoblenou.org>
2017-01-12 15:29:04 +0100

    Agregar DataBinding

--- app/src/main/java/com/example/user/bosseswow/MainActivityFragment.java
+++ app/src/main/java/com/example/user/bosseswow/MainActivityFragment.java
@@ -5,22 +5,70 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.ArrayAdapter;
-
 import java.util.ArrayList;
+import java.util.Arrays;
+import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.util.Log;
+import android.os.AsyncTask;
+import java.util.ArrayList;
+import android.databinding.DataBindingUtil;
 
-/**
- * A placeholder fragment containing a simple view.
- */
+import com.example.user.bosseswow.databinding.FragmentMainBinding;
+import com.example.user.bosseswow.databinding.LvWowRowBinding;
+
+
 public class MainActivityFragment extends Fragment {
+
+    private FragmentMainBinding binding;
 
 
     public MainActivityFragment() {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView lvWow = (ListView) view.findViewById(R.id.lvWow);
+
+        return view;
     }
-}
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_wow_fragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            refresh();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void refresh() {
+    }
+
+
+    }
