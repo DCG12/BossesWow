a6318d3cf45059b19bc1f2492db59ed97e0f89b6
David <46406163y@iespoblenou.org>
2017-01-13 16:07:33 +0100

    Creació del RefreshDataTask

---
+++ app/src/main/java/com/example/user/bosseswow/RefreshDataTask.java
@@ -0,0 +1,53 @@
+package com.example.user.bosseswow;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import com.alexvasilkov.events.Events;
+import java.util.ArrayList;
+
+public class RefreshDataTask {
+
+    private Context context;
+
+
+    RefreshDataTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        Events.post("start-downloading-data");
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+
+        ArrayList<WOW> result;
+
+            result = APIWOW.getInfoBoss();
+
+        Log.d("DEBUG", result != null ? result.toString() : null);
+
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        Dialog dialog = null;
+        dialog.dismiss();
+    }
+
+    public Context getContext() {
+        return context;
+    }
+}
